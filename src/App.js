bd83af375c1f5a4a739e82a48390630baab8f25b
thiago <thiago.heilmann@hotmail.com>
2021-09-30 14:14:43 -0300

    quase pronta

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Botoes from "./botoes.js";
+//13/09/2021 FRONT FINALIZADO, FALTA COLOCAR AS FUNCOES NOS BOTOES
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="main">
+        <header className="header">
+          <h4 className="header">sgurd calculus</h4>
+        </header>
+
+        <section className="res">
+          <h1 id="res">{Botoes.resultado}</h1>
+        </section>
+
+        <section className="calc">
+          <div className="botoes">
+            <button onClick={Botoes.sete}>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button id="res-del">del</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>+</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>-</button>
+            <button>.</button>
+            <button>0</button>
+            <button>/</button>
+            <button>x</button>
+          </div>
+          <div className="botoes-final">
+            <button id="res-del">reset</button>
+            <button id="botao-resultado">=</button>
+          </div>
+        </section>
+      </div>
+    </body>
   );
 }
 
---
+++ src/botoes.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+function Botoes() {
+  let [resultado, setResultado] = useState("");
+
+  function sete() {
+    setResultado(resultado + "7");
+    console.log(7);
+  }
+  //   function sete() {
+  //     setResultado(resultado + "7");
+  //   }
+  //   function sete() {
+  //     setResultado(resultado + "7");
+  //   }
+  //   function sete() {
+  //     setResultado(resultado + "7");
+  //   }
+  //   function sete() {
+  //     setResultado(resultado + "7");
+  //   }
+  //   function sete() {
+  //     setResultado(resultado + "7");
+  //   }
+  //   function sete() {
+  //     setResultado(resultado + "7");
+  //   }
+  //   function sete() {
+  //     setResultado(resultado + "7");
+  //   }
+  //   function sete() {
+  //     setResultado(resultado + "7");
+  //   }
+  //   function sete() {
+  //     setResultado(resultado + "7");
+  //   }
+
+  return Botoes;
+}
+
+export default Botoes;
